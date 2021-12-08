2f78f437e41b52cc4104ea004da8eaf78a5eb4a6
umayanga1997 <umayanga.avi@gmail.com>
2021-12-08 23:39:05 +0530

    movie details view dialog setted

---
+++ src/components/Movie_Details_Viewer.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-row justify="center">
+    <v-dialog
+      v-model="isShow"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
+      <v-card class="dialog-box">
+        <v-toolbar dark>
+          <v-toolbar-title>Move Name</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon class="appBar-icon" @click="dialogAction">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <!-- <v-toolbar-items>
+            <v-btn dark text @click=""> Close </v-btn>
+          </v-toolbar-items> -->
+        </v-toolbar>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "movieDetailsViewer",
+  props: ["isShow"],
+  data() {
+    return {};
+  },
+  methods: {
+    dialogAction() {
+      this.$emit("dialogAction");
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+</style>
